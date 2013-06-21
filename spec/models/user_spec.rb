7f603ad5763202acff68bc63b1fa5f785f13781b
Michael Mitchell <mmitchell@uflavor.com>
2013-06-20 19:17:52 -0700

    add admin bool to user model, add destroy action to user controller

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,14 +10,26 @@ describe User do
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
   it { should respond_to(:remember_token) }
+  it { should respond_to(:admin) }
   it { should respond_to(:authenticate) }
+
   it { should be_valid }
+  it { should_not be_admin }
 
   it { should accept_values_for(:email, "john@example.com", "lambda@gusiev.com") }
   it { should_not accept_values_for(:email, "invalid", nil, "a@b", "john@.com") }
 
   it { should accept_values_for(:name, "john", "dave") }
   it { should_not accept_values_for(:name, "", nil, "a"*51) }
+
+  describe "with admin attribute set to 'true'" do
+    before do
+      @user.save!
+      @user.toggle!(:admin)
+    end
+
+    it { should be_admin }
+  end
 
   describe "when name is not present" do
     before { @user.name = " " }
--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -45,7 +45,7 @@ describe "Authentication" do
     end
   end
 
-describe "authorization" do
+  describe "authorization" do
 
     describe "for non-signed-in users" do
       let(:user) { FactoryGirl.create(:user) }
@@ -98,6 +98,18 @@ describe "authorization" do
       describe "submitting a PUT request to the Users#update action" do
         before { put user_path(wrong_user) }
         specify { response.should redirect_to(root_path) }
+      end
+    end
+
+    describe "as non-admin user" do
+      let(:user) { FactoryGirl.create(:user) }
+      let(:non_admin) { FactoryGirl.create(:user) }
+
+      before { sign_in non_admin }
+
+      describe "submitting a DELETE request to the Users#destroy action" do
+        before { delete user_path(user) }
+        specify { response.should redirect_to(root_path) }        
       end
     end
   end
